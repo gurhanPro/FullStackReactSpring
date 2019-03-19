c0fe741dc009c8e55060513e376e49b5c9211c27
ziyaad <gurhanpro@gmail.com>
2019-03-19 08:45:52 +0200

    implemented a post method to register a project in the database

--- springOne/src/main/java/com/inclusivity/springOne/service/ProjectTaskService.java
+++ springOne/src/main/java/com/inclusivity/springOne/service/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.inclusivity.springOne.service;
 
+import com.inclusivity.springOne.domain.ProjectTask;
 import com.inclusivity.springOne.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,14 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
+
+    //a method that saves our project data
+    public ProjectTask saveOrUpadateprojectTask(ProjectTask projectTask){
+        if(projectTask.getStatus()==null || projectTask.getStatus() ==""){
+            projectTask.setStatus("TO DO");
+
+        }
+        return projectTaskRepository.save(projectTask);
+    }
 }
+//jdbc:h2:mem:testdb
--- springOne/src/main/java/com/inclusivity/springOne/web/ProjectTaskController.java
+++ springOne/src/main/java/com/inclusivity/springOne/web/ProjectTaskController.java
@@ -1,10 +1,17 @@
 package com.inclusivity.springOne.web;
 
+import com.inclusivity.springOne.domain.ProjectTask;
 import com.inclusivity.springOne.service.ProjectTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/board")
@@ -12,4 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectTaskController {
     @Autowired
     private ProjectTaskService projectTaskService;
+
+    @PostMapping("")
+    public ResponseEntity<?> addPTToBoard(@Valid @RequestBody ProjectTask projectTask, BindingResult result){
+
+        // this is to usefull errors when posting goes wrong 
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
+        ProjectTask newPT = projectTaskService.saveOrUpadateprojectTask(projectTask);
+        return new ResponseEntity<ProjectTask>(newPT, HttpStatus.CREATED);
+    }
 }
